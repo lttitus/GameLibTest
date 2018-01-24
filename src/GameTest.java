36348ce2cd225fc4b71bce64ae84efca55f54d7c
lttitus <greenslimy@live.com>
2018-01-24 01:34:02 -0600

    Refactor and Recode
    Removed large portions of changed framwork code and almost entirely remove all anonymous class related memory leaks.

--- src/GameTest.java
+++ src/GameTest.java
@@ -3,9 +3,7 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 import com.greenslimy.gamelib.GUIGame;
-import com.greenslimy.gamelib.display.graphics.shapes.Rectangle;
 import com.greenslimy.gamelib.display.graphics.text.Text;
-import com.greenslimy.gamelib.display.graphics.text.TimedText;
 
 /**
  * A test game to help code and show the power of the GameLib.
@@ -16,7 +14,7 @@ public class GameTest extends GUIGame {
 	
 	private Player player;
 	
-	private final Rectangle borderSelector = new Rectangle(Color.GREEN, 0, 0, 0, 0) {
+	/*private final Rectangle borderSelector = new Rectangle(Color.GREEN, 0, 0, 0, 0) {
 		public void updateRectangle() {
 			destroy = !mLeftPressed;
 		}
@@ -32,13 +30,19 @@ public class GameTest extends GUIGame {
 			destroy = false;
 		}
 	};
-	private Rectangle selector;
+	private Rectangle selector;*/
+	
+	private Text counterFPS = new Text("FPS: ", 0, 10);
+	private Text mouseTracker = new Text("Mouse: ", 0, 25);
 	
 	public GameTest(String title, int w, int h) throws IOException {
 		super(title, w, h);
 		this.player = new Player(w/2, h/2, 32, 32);	//Instantiate the player
+		addDrawable(counterFPS);
 		addDrawable(player);
-		addDrawable(new Text("FPS: ", 0, 10) {
+		addDrawable(mouseTracker);
+		
+		/*addDrawable(new Text("FPS: ", 0, 10) {
 			public void update() {
 				text = "FPS: "+fps;
 			}
@@ -53,20 +57,23 @@ public class GameTest extends GUIGame {
 		addDrawable(new Text(Color.RED, "Player", player.x, player.y) {
 			public void update() {
 				x = player.x;
-				y = player.y-10;	//Offset
+				y = player.y-5;	//Offset above the head
 			}
 		});
 		addDrawable(new TimedText("This is a test", 0, h/2, 5) {
 			public void updateText() {
 				y--;
 			}
-		});
+		});*/
 	}
 	
+	private int mcx, mcy;
 	protected void logicUpdate() {
-		if(mPressed) {
+		mcx = getMousePosition().width;
+		mcy = getMousePosition().height;
+		/*if(mPressed) {
 			selector.changePoints(mpx, mpy, mcx-mpx, mcy-mpy);
-		}
+		}*/
 		if(keysPressed[KeyEvent.VK_DOWN]) {	//If we have the down arrow pressed
 			player.y++;
 		}if(keysPressed[KeyEvent.VK_UP]) {	//If we have the up arrow pressed
@@ -76,18 +83,17 @@ public class GameTest extends GUIGame {
 		}if(keysPressed[KeyEvent.VK_LEFT]) {	//If we have the left arrow pressed
 			player.x--;
 		}
+		
+		counterFPS.update("FPS: "+fps);
+		mouseTracker.update("Mouse: "+mcx+", "+mcy, mcx, mcy);
 	}
-
-	protected void leftClicked() {
-		selector = borderSelector;
+	
+	protected void mousePress() {
+		mouseTracker.update(Color.RED);
 	}
-
-	protected void rightClicked() {
-		selector = filledSelector;
-	}
-
-	protected void mouseClicked() {
-		addDrawable(selector);
+	
+	protected void mouseRelease() {
+		mouseTracker.update(Color.WHITE);
 	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.io.IOException;
 
 import com.greenslimy.gamelib.display.graphics.sprites.Sprite;
+import com.greenslimy.gamelib.display.graphics.text.Text;
 
 public class Player extends Sprite {
+	
+	private Text name = new Text(Color.GREEN, "Player", 0, 0);
 
 	public Player(int x, int y, int w, int h) throws IOException {
 		super("res/player.png", x, y, w, h);
@@ -10,7 +15,14 @@ public class Player extends Sprite {
 	}
 
 	public void update() {
-		
+		name.x = x;
+		name.y = y-10;
+	}
+	
+	@Override
+	public void draw(Graphics g) {
+		super.draw(g);
+		name.draw(g);
 	}
 
 }
